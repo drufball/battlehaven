40fa2ae2a4981cf72c90efed2a536d66ee5a518f
Vivian Qu <qu@pinterest.com>
2018-02-03 21:18:49 -0800

    Add alert dialog for starting a new battle

--- lib/addMonster.dart
+++ lib/addMonster.dart
@@ -4,46 +4,46 @@ class AddMonsterModalSheet extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new Container(
-//        padding: const EdgeInsets.all(0.0),
-//        margin: const EdgeInsets.all(0.0),
         child: new Padding(
           padding: const EdgeInsets.all(32.0),
           child: new Column(
               mainAxisSize: MainAxisSize.min,
-              children: _buttons()
+              children: _buttons(context)
           )
         )
-//        child: new Padding(
-//            padding: const EdgeInsets.all(10.0),
-//            child: new Column(
-//                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-//                children: _buttons())
-//        )
     );
   }
 
-  List<Widget> _buttons() {
+  List<Widget> _buttons(BuildContext context) {
     return [
       new Row(
           children: [new Expanded(
-              child: _styledButton('Add Monster class')
+              child: _styledButton(
+                'Add Monster class',
+                buttonPressed: () {
+
+                }
+              )
           )]
       ),
       new Row(
           children: [new Expanded(
-              child: _styledButton('Start new battle')
+              child: _styledButton(
+                'Start new battle',
+                buttonPressed: () {
+                  _showAlert(context);
+                }
+              )
           )]
       ),
     ];
   }
 
-  Widget _styledButton(String text) {
+  Widget _styledButton(String text, {VoidCallback buttonPressed}) {
     return new Padding(
         padding: const EdgeInsets.only(bottom: 16.0),
         child: new RaisedButton(
-          onPressed: () {
-            // Add button behavior here
-          },
+          onPressed: buttonPressed,
           child: new Text(
               text,
               style: new TextStyle(color: Colors.white)
@@ -52,6 +52,36 @@ class AddMonsterModalSheet extends StatelessWidget {
           highlightColor: const Color.fromRGBO(21, 101, 192, 1.0),
           splashColor: const Color.fromRGBO(21, 101, 192, 1.0),
         )
+    );
+  }
+
+  void _showAlert(BuildContext context) {
+    return showDialog<Null>(
+      context: context,
+      barrierDismissible: false, // user must tap button!
+      child: new AlertDialog(
+        title: new Text('Start new battle'),
+        content: new SingleChildScrollView(
+          child: new Text('Are you sure you want to start over? '
+              'This will remove all current monster classes, '
+              'as well as accumulated status and damage.'),
+        ),
+        actions: <Widget>[
+          new FlatButton(
+            child: new Text('Go back'),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          ),
+          new FlatButton(
+            child: new Text('Continue'),
+            onPressed: () {
+              // Reset all values here
+              Navigator.of(context).pop();
+            },
+          ),
+        ],
+      ),
     );
   }
 }
